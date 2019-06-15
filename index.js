c78c34cd2862903bfa9303432974a705e87d4515
Logan Praznik <lpraz@mts.net>
2019-06-15 18:50:26 -0500

    Implemented marking tasks as (in)complete

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ import { h, app } from "hyperapp"
 // TODO: search
 // TODO: autosave to local storage
 
+const toggleCompleted = (state, index) => ({
+    ...state,
+    items: state.items.map((item, mapIndex) => (
+        index === mapIndex ?
+        {
+            ...item,
+            completed: !item.completed
+        } :
+        item
+    ))
+});
+
 app({
     init: {
         items: [
             {
                 name: "Learn Hyperapp",
-                completed: false
+                completed: true
             },
             {
                 name: "Learn Vue",
@@ -23,11 +35,11 @@ app({
     view: state => (
         <div>
             <ul>
-                {state.items.map(item =>
+                {state.items.map((item, index) =>
                     <li>
-                        <span>{item.name}</span>
+                        <span class={item.completed ? "completed" : ""}>{item.name}</span>
                         <br />
-                        <a class="button complete">&#x2714;</a>
+                        <a class="button complete" onclick={[toggleCompleted, index]}>&#x2714;</a>
                         <a class="button edit">&#x270F;</a>
                         <a class="button delete">&#x2716;</a>
                         <a class="button up">&#x2197;</a>
