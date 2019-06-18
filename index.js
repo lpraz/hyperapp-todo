f1b2d52040edcf47a5e6aeddc3c154c7352228b6
Logan Praznik <lpraz@mts.net>
2019-06-17 21:40:34 -0500

    Implemented adding new tasks

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { h, app } from "hyperapp"
 
-// TODO: confirm remove
-// TODO: search
+// TODO: search/filter
 // TODO: autosave to local storage
+
+const onInputValue = event =>
+    event.target.value;
 
 const toggleCompleted = (state, index) => ({
     ...state,
@@ -18,34 +20,63 @@ const toggleCompleted = (state, index) => ({
 
 const promptRemove = (state, index) => ({
     ...state,
-    promptRemoveFor: index
+    removingItem: index
 });
 
 const cancelRemove = state => ({
     ...state,
-    promptRemoveFor: null
+    removingItem: null
 });
 
 const remove = (state, index) => ({
     ...state,
     items: state.items.filter((_, mapIndex) => (index !== mapIndex)),
-    promptRemoveFor: null
+    removingItem: null
+});
+
+const promptAdd = state => ({
+    ...state,
+    addingItem: true
+});
+
+const setNewItemName = (state, name) => ({
+    ...state,
+    newItemName: name
+});
+
+const cancelAdd = state => ({
+    ...state,
+    addingItem: false,
+    newItemName: ""
+});
+
+const add = (state, newItemName) => ({
+    ...state,
+    items: [
+        ...state.items,
+        {
+            name: newItemName,
+            completed: false
+        }
+    ],
+    addingItem: false,
+    newItemName: ""
 });
 
 const promptRemoveAll = state => ({
     ...state,
-    promptRemoveFor: "all"
+    removingItem: "all"
 });
 
 const cancelRemoveAll = state => ({
     ...state,
-    promptRemoveFor: null
+    removingItem: null
 });
 
 const removeAll = state => ({
     ...state,
     items: [],
-    promptRemoveFor: null
+    removingItem: null
 });
 
 app({
@@ -60,11 +91,14 @@ app({
                 completed: false
             },
             {
-                name: "This is a really long task meant to show off what multiple lines looks like",
+                name: "This is a really long task meant to show off what " +
+                    "having multiple lines looks like",
                 completed: false
             }
         ],
-        promptRemoveFor: null
+        removingItem: null,
+        addingItem: false,
+        newItemName: null
     },
     view: state => (
         <div>
@@ -73,11 +107,13 @@ app({
                     <li class={item.completed ? "completed" : ""}>
                         <span>{item.name}</span>
                         <br />
-                        <a class="button complete" onclick={[toggleCompleted, index]}>
+                        <a
+                            class="button green"
+                            onclick={[toggleCompleted, index]}>
                             &#x2714;
                         </a>
-                        <a class="button edit">&#x270F;</a>
-                        <a class="button remove" onclick={[promptRemove, index]}>
+                        <a class="button yellow">&#x270F;</a>
+                        <a class="button red" onclick={[promptRemove, index]}>
                             &#x2716;
                         </a>
                         <a class="button">&#x2197;</a>
@@ -86,33 +122,63 @@ app({
                 )}
             </ul>
             <p>
+                <a
+                    class="button blue"
+                    onclick={promptAdd}>
+                    + Add
+                </a>
                 {state.items.length ? (
                     <a
-                        class="button remove"
+                        class="button red"
                         onclick={promptRemoveAll}>
                         &#x2716; Remove All
                     </a>
                 ) : ""}
             </p>
-            {![null, "all"].includes(state.promptRemoveFor) ? (
+            {state.addingItem ? (
+                <div class="modal">
+                    <p>Enter a name for the task:</p>
+                    <input
+                        type="text"
+                        value={state.newItemName}
+                        onInput={[setNewItemName, onInputValue]} />
+                    <br />
+                    <a class="button" onclick={cancelAdd}>&#x21A9; Cancel</a>
+                    <a
+                        class="button blue"
+                        onclick={[add, state.newItemName]}>
+                        + Add
+                    </a>
+                </div>
+            ): ""}
+            {![null, "all"].includes(state.removingItem) ? (
                 <div class="modal">
                     <p>
                         Are you sure you want to remove the task
-                        "{state.items[state.promptRemoveFor].name}"?
+                        "{state.items[state.removingItem].name}"?
                     </p>
-                    <a class="button" onclick={cancelRemove}>Cancel</a>
+                    <a class="button" onclick={cancelRemove}>
+                        &#x21A9; Cancel
+                    </a>
                     <a
-                        class="button remove"
-                        onclick={[remove, state.promptRemoveFor]}>
+                        class="button red"
+                        onclick={[remove, state.removingItem]}>
                         &#x2716; Remove
                     </a>
                 </div>
             ) : ""}
-            {state.promptRemoveFor === "all" ? (
+            {state.removingItem === "all" ? (
                 <div class="modal">
-                    <p>Are you sure you want to remove all tasks from the list?</p>
-                    <a class="button" onclick={cancelRemoveAll}>Cancel</a>
-                    <a class="button remove" onclick={[removeAll]}>&#x2716; Remove All</a>
+                    <p>
+                        Are you sure you want to remove all tasks from the
+                        list?
+                    </p>
+                    <a class="button" onclick={cancelRemoveAll}>
+                        &#x21A9; Cancel
+                    </a>
+                    <a class="button red" onclick={[removeAll]}>
+                        &#x2716; Remove All
+                    </a>
                 </div>
             ) : ""}
         </div>
